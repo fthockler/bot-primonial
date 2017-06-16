818643b01e447771e84ccb4aec1c898b34708c04
fthockler <fthockler@salesforce.com>
2017-06-16 17:50:46 +0200

    Help + new query

--- modules/handlers.js
+++ modules/handlers.js
@@ -60,5 +60,5 @@ exports.hi = (sender) => {
 };
 
 exports.help = (sender) => {
-    messenger.send({text: `You can ask me questions like "Find houses in Boston", "3 bedrooms in Boston", "3 bedrooms in Boston between 500000 and 750000", "show me price changes"`}, sender);
+    messenger.send({text: `Vous pouvez me poser des questions comme "je cherche un programme pinel" ou "conseil pinel"`}, sender);
 };
